f9b433e0cd9956bfe13f249da4461cf3b3cdcbad
orewaee <orewaee@gmail.com>
2024-09-07 16:47:38 +0400

    feat: address flag added

--- cmd/quotes/main.go
+++ cmd/quotes/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
 	"github.com/orewaee/quotes/internal/controllers"
 	"github.com/orewaee/quotes/internal/disk"
 	"github.com/orewaee/quotes/internal/services"
 )
 
 func main() {
+	addr := ""
+
+	flag.StringVar(&addr, "addr", ":8080", "--addr=:8081")
+	flag.Parse()
+
 	quoteRepo := disk.NewQuoteRepo()
 	quoteApi := services.NewQuoteService(quoteRepo)
 
-	controller := controllers.NewRestController(":8080", quoteApi)
+	controller := controllers.NewRestController(addr, quoteApi)
 
 	if err := controller.Run(); err != nil {
 		panic(err)
