fef515e477be0d632943f17f291fc7a5fad7d072
orewaee <orewaee@gmail.com>
2024-09-07 17:28:20 +0400

    feat: added logger

--- internal/controllers/rest.go
+++ internal/controllers/rest.go
@@ -5,6 +5,7 @@ import (
 	"github.com/orewaee/quotes/internal/app/api"
 	"github.com/orewaee/quotes/internal/dtos"
 	"github.com/orewaee/quotes/internal/utils"
+	"github.com/rs/zerolog"
 	"net/http"
 	"time"
 )
@@ -12,9 +13,10 @@ import (
 type RestController struct {
 	server   *http.Server
 	quoteApi api.QuoteApi
+	logger   *zerolog.Logger
 }
 
-func NewRestController(addr string, quoteApi api.QuoteApi) *RestController {
+func NewRestController(addr string, quoteApi api.QuoteApi, logger *zerolog.Logger) *RestController {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /random", func(writer http.ResponseWriter, request *http.Request) {
@@ -42,13 +44,16 @@ func NewRestController(addr string, quoteApi api.QuoteApi) *RestController {
 	return &RestController{
 		server:   server,
 		quoteApi: quoteApi,
+		logger:   logger,
 	}
 }
 
 func (controller *RestController) Run() error {
+	controller.logger.Info().Msgf("running app at addr %s", controller.server.Addr)
 	return controller.server.ListenAndServe()
 }
 
 func (controller *RestController) Shutdown(ctx context.Context) error {
+	controller.logger.Info().Msg("shutting down the app...")
 	return controller.server.Shutdown(ctx)
 }
